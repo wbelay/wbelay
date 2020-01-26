ee3cc7b106caec89a2d0c72d3b870ce9007f2f1f
wbelay <36802465+wbelay@users.noreply.github.com>
2020-01-26 18:09:06 -0500

    Add files via upload

---
+++ testM.js
@@ -0,0 +1,67 @@
+﻿// JavaScript source code
+var listName;
+
+function main() {
+
+
+      $.ajax({
+
+             url: _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getByTitle('testABC')/items?$select=Name",
+             contentType: "application/json;odata=verbose",
+             headers: { "Accept": "application/json; odata=verbose" },
+             success: OnDataReturned,
+             error: onError
+
+      
+          });
+
+    function onError(err) {
+
+        console.log('%cRequest  accessing list.', 'color: red');
+
+    }
+
+    function OnDataReturned(data) {   
+    
+                     var odataResults = data.d.results;
+                     var dataArray = [];
+                     
+
+                      if(odataResults.length > 0)
+                      {
+                      
+                         for(var i=0; i < odataResults.length; i++)
+                         {
+                         
+                           var _obj = {
+                                         Description: odataResults[i].Name["Description"],
+                                         Url:  odataResults[i].Name["Url"]
+                           
+                               };
+                         
+                            dataArray.push(_obj);
+                         }
+                         
+                      
+                      }
+                      
+                      
+                      
+           var data = { 
+   
+                           DataDetail: dataArray
+                 
+                       };
+                       
+                       
+          $('#detailSection').html($('#displayData').render(data));  
+
+
+
+                   }
+                   
+
+
+}
+
+
